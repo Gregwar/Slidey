78cd7950c50be1869b69813b2d2af3197085ca80
Gregwar <g.passault@gmail.com>
2012-06-15 15:16:54 +0200

    Adding favicon support

--- src/templates/layout.php
+++ src/templates/layout.php
@@ -3,9 +3,10 @@
     <head>
 	<meta charset="utf-8" />
 	<title><?php echo $slidey->title(); ?></title>
-	<link type="text/css" media="screen" rel="stylesheet" href="slidey/css/style.css" />
+        <link type="text/css" media="screen" rel="stylesheet" href="slidey/css/style.css" />
+        <link rel="shortcut icon" href="favicon.ico" />
 	<script type="text/javascript" src="slidey/js/jquery.js"></script>
-	<script type="text/javascript" src="slidey/js/slidey.js"></script>
+        <script type="text/javascript" src="slidey/js/slidey.js"></script>
 	<?php echo $slidey->header(); ?>
     </head>
     <body>
