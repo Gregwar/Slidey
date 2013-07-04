0b6f2ac76067fc102a7ce2acd54ecd93ff270d17
Gregwar <g.passault@gmail.com>
2013-07-05 01:19:52 +0200

    Fixing toc

--- Factory.php
+++ Factory.php
@@ -4,6 +4,8 @@ namespace Gregwar\Slidey;
 
 use Gregwar\RST\HTML\Factory as Base;
 
+use Gregwar\RST\HTML\Directives\Wrap;
+
 class Factory extends Base
 {
     public function getDirectives()
@@ -11,13 +13,16 @@ class Factory extends Base
         $directives = parent::getDirectives();
         $directives[] = new Directives\Slide;
         $directives[] = new Directives\Math;
+        $directives[] = new Wrap('textOnly');
+        $directives[] = new Wrap('slideOnly');
+        $directives[] = new Wrap('discover');
 
         return $directives;
     }
 
     public function getClass($name)
     {
-        $nodes = array('CodeNode');
+        $nodes = array('CodeNode', 'TocNode');
 
         if (in_array($name, $nodes)) {
             return '\\Gregwar\\Slidey\\Nodes\\'.$name;
---
+++ Nodes/TocNode.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Gregwar\Slidey\Nodes;
+
+use Gregwar\RST\HTML\TocNode as Base;
+
+class TocNode extends Base
+{
+    public function renderLevel($url, $titles, $level = 1, $path = array())
+    {
+        $render = parent::renderLevel($url, $titles, $level, $path);
+
+        if ($level == 1) {
+            $render = '<div class="slide middleSlide tocSlide">'.$render.'</div>';
+        }
+
+        return $render;
+    }
+}
