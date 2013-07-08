bf6450a3882538572026eb242692783b644f21ab
Gregwar <g.passault@gmail.com>
2013-07-08 13:59:41 +0200

    Adding important

--- Factory.php
+++ Factory.php
@@ -16,7 +16,7 @@ class Factory extends Base
         $directives[] = new Directives\DiscoverList;
 
         $classes = array('textOnly', 'slideOnly', 'discover',
-            'step', 'note', 'tip', 'warning', 'spoiler', 'center');
+            'step', 'note', 'tip', 'warning', 'spoiler', 'center', 'important');
 
         foreach ($classes as $class) {
             $directives[] = new Wrap($class);
