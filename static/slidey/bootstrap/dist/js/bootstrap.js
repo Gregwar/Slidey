4de167e9214a5042a363934765581cbd50ff4ea5
Gregwar <g.passault@gmail.com>
2018-07-11 10:18:40 +0200

    Minor color change

--- static/slidey/bootstrap/dist/js/bootstrap.js
+++ static/slidey/bootstrap/dist/js/bootstrap.js
@@ -1,6 +1,6 @@
 /**
 * bootstrap.js v3.0.0 by @fat and @mdo
-* Copyright 2013 Twitter Inc.
+* Copyright 2018 Twitter Inc.
 * http://www.apache.org/licenses/LICENSE-2.0
 */
 if (!jQuery) { throw new Error("Bootstrap requires jQuery") }
