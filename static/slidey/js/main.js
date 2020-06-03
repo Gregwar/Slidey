18dae9b17a6650e0e1224f1e85870413b2e7bf08
Gregwar <g.passault@gmail.com>
2020-06-03 20:10:41 +0200

    Minor fixes

--- static/slidey/js/main.js
+++ static/slidey/js/main.js
@@ -2,7 +2,7 @@ var slidey = new Slidey();
 new SlideyMenuExtension(slidey);
 new SlideyImagesExtension(slidey);
 new SlideySpoilersExtension(slidey);
-new SlideyMobileExtension(slidey);
+// new SlideyMobileExtension(slidey);
 new SlideyStepsExtension(slidey);
 new SlideyPermalinkExtension(slidey);
 if (typeof(SlideyInteractiveExtension) != 'undefined') {
