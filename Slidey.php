152a8cbc7bc382e8e6008c37f05ebfcce0c2f49e
Grégoire Passault <g.passault@gmail.com>
2024-11-06 09:59:02 +0100

    Removing polyfill.io

--- Slidey.php
+++ Slidey.php
@@ -121,7 +121,6 @@ class Slidey extends Builder
         
             // Mathjax
             $document->addHeaderNode(new RawNode('
-                <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
                 <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
             '));
 
